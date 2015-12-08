248a2620fd1a664ae0873e9d7d087a0d37b0f58a
KamilKrol <kamil.h.krol@gmail.com>
2015-12-08 22:40:48 +0100

    generic stacks.

---
+++ 8-generic-types-methods/src/Main1.scala
@@ -0,0 +1,15 @@
+import stacks._
+
+/**
+  * Created by kamil on 08.12.15.
+  */
+object Main1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val stack = new EmptyStack
+    stack.pop
+
+  }
+
+}
---
+++ 8-generic-types-methods/src/stacks/package.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by kamil on 08.12.15.
+  */
+
+package object stacks {
+
+  abstract class Stack[T] {
+    def push(x: T): Stack[T] = new NotEmptyStack(x, this)
+
+    def isEmpty: Boolean
+
+    def top: T
+
+    def pop: Stack[T]
+  }
+
+  class EmptyStack[T] extends Stack[T] {
+    override def isEmpty: Boolean = true
+
+    override def top: T = sys.error("EmptyStack.top")
+
+    override def pop: Stack[T] = sys.error("EmptyStack.pop")
+  }
+
+  class NotEmptyStack[T](elem: T, rest: Stack[T]) extends Stack[T] {
+    override def isEmpty: Boolean = false
+
+    override def top: T = elem
+
+    override def pop: Stack[T] = rest
+  }
+
+}
