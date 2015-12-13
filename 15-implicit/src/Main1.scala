f6ef4bb9044be29a65dcf6626691701db15ddfdd
KamilKrol <kamil.h.krol@gmail.com>
2015-12-13 11:54:49 +0100

    Implicit conversions.

--- 15-implicit/src/Main1.scala
+++ 15-implicit/src/Main1.scala
@@ -5,8 +5,13 @@ object Main1 {
 
   def main(args: Array[String]): Unit = {
 
-
+    val double = 1.5
+    printInt(double)
 
   }
+
+  implicit def doubleToInt(double: Double): Int = double.toInt
+
+  def printInt(value: Int) = println("int = " + value)
 
 }
